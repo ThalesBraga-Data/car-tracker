43d5100256922e0519d4ec5d1964cb9c10cc259e
ThalesBraga-Data <thaleslbraga2@gmail.com>
2026-03-13 17:15:52 -0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,6 +1,5 @@
 import requests
 import os
-from bs4 import BeautifulSoup
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
@@ -12,28 +11,4 @@ def enviar(msg):
         "text": msg
     })
 
-url = "https://www.webmotors.com.br/carros/estoque/fiat/argo?tipoveiculo=carros&cidade=Campinas"
-
-headers = {"User-Agent": "Mozilla/5.0"}
-
-r = requests.get(url, headers=headers)
-
-soup = BeautifulSoup(r.text, "html.parser")
-
-carros = soup.find_all("a")
-
-links = []
-
-for c in carros:
-    link = c.get("href")
-    if link and "/comprar/" in link:
-        links.append("https://www.webmotors.com.br" + link)
-
-links = list(set(links))
-
-if links:
-    enviar("🚗 Carros encontrados:")
-    for l in links[:5]:
-        enviar(l)
-else:
-    enviar("Nenhum carro encontrado agora.")
+enviar("TESTE NOVO SCRIPT 🚗")
