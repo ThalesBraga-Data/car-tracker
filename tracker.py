884049f55c3e76d8b207276d14abfcdd0ad090de
ThalesBraga-Data <thaleslbraga2@gmail.com>
2026-03-13 16:59:51 -0300

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from bs4 import BeautifulSoup
 
 TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
@@ -11,4 +12,28 @@ def enviar(msg):
         "text": msg
     })
 
-enviar("🚗 Bot iniciado com sucesso no GitHub!")
+url = "https://www.webmotors.com.br/carros/estoque/fiat/argo?tipoveiculo=carros&cidade=Campinas"
+
+headers = {
+    "User-Agent": "Mozilla/5.0"
+}
+
+r = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(r.text, "html.parser")
+
+carros = soup.find_all("a")
+
+encontrados = []
+
+for c in carros:
+    link = c.get("href")
+    if link and "/comprar/" in link:
+        encontrados.append("https://www.webmotors.com.br" + link)
+
+if encontrados:
+    enviar("🚗 Carros encontrados:")
+    for c in encontrados[:5]:
+        enviar(c)
+else:
+    enviar("Nenhum carro encontrado agora.")
